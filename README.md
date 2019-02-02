a4322cff46664a0859276c9b2ec0f369a941f8ce
lordzurp@mac <lordzurp@lastmail.xyz>
2019-02-02 10:50:59 +0100

    comments + readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# fan_faker
+# Fan Faker
 
 [![GitHub release](http://img.shields.io/badge/Version-1.0-brightgreen.svg?style=flat)][release]
 [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)][license] 
@@ -9,7 +9,16 @@
 [license]: https://raw.githubusercontent.com/lordzurp/DishWarrior/master/LICENSE
 [arduino]: https://www.arduino.cc/en/Guide/ArduinoNano
 
+## Principe de fonctionnement
+Un ventilateur PC a un tachymètre qui envoie 2 pulses par tour
+Pour le simuler, il suffit d'envoyer un signal carré à la fréquence correspondante
 
+#####exemple : on veut simuler un ventilateur tournant à 6000 rpm à 12V
+- on mesure la tension d'alimentation du ventilo, via un pont diviseur 1/3 : on passe de 0-15V à 0-5V
+- un map la valeur sur une echelle de 0 à 10.000 (vitesse max théorique à 15V)
+- on passe en Hertz : 10.000 / 60 = 166.67 Hz
+- on veut 2 pulses par tour, donc on multiplie par 2 -> 333 pulses / seconde
+- on utilise la librairie Tone() pour génerer un signal carré à la fréquence souhaitée
 
 ### Historique
 
--- fan_faker.ino
+++ fan_faker.ino
@@ -8,12 +8,10 @@ lordzurp
 int sensorPin = A0;  // mesure tension entrée
 int outputPin = 5;   // sortie signal carré
 int heartbeat = 9;   // sortie LED
-int led = 13;        // internal LED
 
 unsigned int sensorValueRaw = 0;  // valeur tension entrée brute
 float sensorValue = 0;            // valeur tension étalonnée
-
-float fake_rpm = 0;       // fréquence sortie
+float fake_rpm = 0;     		  // fréquence sortie
 
 void setup()  {
   pinMode(outputPin, OUTPUT);
@@ -27,12 +25,15 @@ void setup()  {
 
 void loop()  {
     
-    sensorValueRaw = analogRead(sensorPin);
-    sensorValue = map(sensorValueRaw, 0, 1023, 0, 15000)/1000.0;
-   
-    fake_rpm = map(sensorValueRaw, 0, 1023, 0, 10000);
-    tone(outputPin, fake_rpm/30);
+    sensorValueRaw = analogRead(sensorPin);		// on lit la tension d'entrée, qui est divisée par 3 (0-15V -> 0-5V)
+    fake_rpm = map(sensorValueRaw, 0, 1023, 0, 10000);	// on ajuste la mesure sur la vitesse voulue du ventilo (0-10.000 rpm)
+    tone(outputPin, fake_rpm/60*2);	// on envoie une pulse correspondant à la frequence du tachy : 10.000 rpm / 60s / 2 pulse par tour
 
-    digitalWrite(heartbeat, !digitalRead(heartbeat));
+	/* debug
+    sensorValue = map(sensorValueRaw, 0, 1023, 0, 15000)/1000.0;	// on remet la valeur mesurée sur une echelle 0-15V
+	Serial.println(sensorValue);		// on envoie la mesure sur le prot série, pour debug
+	*/
+
+    digitalWrite(heartbeat, !digitalRead(heartbeat));	// heartbeat, pour verifier que le micro boucle toujours
     delay(500);
 }
