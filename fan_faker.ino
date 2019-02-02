7d78ac84c1d2b3ca3568db45dbaa6957a6db962e
lordzurp@mac <lordzurp@lastmail.xyz>
2019-02-02 10:26:20 +0100

    clean heartbeat

--- fan_faker.ino
+++ fan_faker.ino
@@ -27,14 +27,12 @@ void setup()  {
 
 void loop()  {
     
-    digitalWrite(heartbeat, HIGH);
     sensorValueRaw = analogRead(sensorPin);
     sensorValue = map(sensorValueRaw, 0, 1023, 0, 15000)/1000.0;
    
     fake_rpm = map(sensorValueRaw, 0, 1023, 0, 10000);
     tone(outputPin, fake_rpm/30);
 
-    delay(100);               // wait for a second
-    digitalWrite(heartbeat, LOW);    // turn the LED off by making the voltage LOW
-    delay(900);        
+    digitalWrite(heartbeat, !digitalRead(heartbeat));
+    delay(500);
 }
